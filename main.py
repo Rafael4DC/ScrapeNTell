720f9d5b4a4bfbb1454f565775405c167d9f24c6
Dragoral <dragoral.wizard@gmail.com>
2023-05-13 03:04:43 +0100

    Works from CMD

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
+import datetime
 from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import discord
 from discord.ext import commands
+import asyncio
 
 # This generates options automatically from the options.py file
 # and passes them to the Chrome class automatically
@@ -23,6 +24,7 @@ WINDOW_SIZE = "1920,1080"
 notify_list = []
 chrome_options = Options()
 chrome_options.add_argument("--headless=new")
+time_last = datetime.datetime.now()
 
 
 # event that triggers when the bot is ready to receive commands
@@ -31,39 +33,51 @@ async def on_ready():
     print('Bot is ready')
 
 
-
-# command to send a private message
 @client.command()
-async def hook_me_to_the_main_frame(ctx):
+async def ping_me(ctx):
     user = ctx.author
     if user in notify_list:
         await user.send("NT dingus")
     else:
         notify_list.append(user)
-        await user.send("You have been added to the notification list.")
-        new_text = ""
-        old_text = ""
-        while True:
-            old_text = new_text
-            driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=chrome_options)
 
-            url = 'https://www.warhammer-community.com/en-us/latest-news-features/'
 
-            # to open the url in the browser
-            driver.get(url)
-            elements = driver.find_elements(By.XPATH, '/html/body/div[1]/div[3]/div[2]')
-            for element in elements:
-                new_text = element.text
+async def website_check(ctx):
+    global time_last
+    new_text = ""
+    old_text = ""
+    while True:
+        old_text = new_text
+        driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=chrome_options)
 
-            # Close Chrome
-            driver.close()
+        url = 'https://www.warhammer-community.com/en-us/latest-news-features/'
 
-            if new_text == old_text:
+        # to open the url in the browser
+        driver.get(url)
+        elements = driver.find_elements(By.XPATH, '/html/body/div[1]/div[3]/div[2]')
+        for element in elements:
+            new_text = element.text
+
+        # Close Chrome
+        driver.close()
+
+        if new_text == old_text:
+            if time_last + datetime.timedelta(seconds=600) <= datetime.datetime.now():
+                channel = client.get_channel(1106723442465718352)
+                await channel.send('Big Chilling as of ' + str(datetime.datetime.now()))
                 print("The lists are equal")
-            else:
-                user = ctx.author
+                time_last = datetime.datetime.now()
+        else:
+            for user in notify_list:
                 await user.send("RAPIDO VAI VER https://www.warhammer-community.com/en-us/latest-news-features/")
-            sleep(1000)#time in seconds
+        sleep(10)  # time in seconds
+        print("Still sleeping")
+        sleep(100)
+
+@client.command()
+async def do_monitor(ctx):
+    asyncio.create_task(website_check(ctx))
+    print("monitor started")
 
 
 client.run('MTEwNjM3NjQ0MjEyOTk0MDQ5MA.GbJBMa.OvyRJA-TNhPHyQe1bUGbob0h3YG2hugSztvQ6M')
